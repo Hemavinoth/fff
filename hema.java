78251653cf25750820e0881713e7c08731f2db83
hemalatha <avhemavinoth@gmail.com>
2016-09-18 22:40:54 +0530

    Create hema.java

---
+++ hema.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class hema {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		if(n>0)
+		 System.out.println("Number is Positive");
+		if(n<0)
+		  System.out.println("Number is Negative");
+		if(n==0)
+		 System.out.println( "Number is ZERO");
+	}
+
+}
